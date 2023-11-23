46c836a1f377e974458bed54fea7dcbb2d73b6b9
Matheus Bazoti <102468065+bazotimatheus@users.noreply.github.com>
2023-11-23 19:55:48 -0300

    Update and rename dismiss.py to layoff.py

--- peter/layoff.py
+++ peter/layoff.py
@@ -1,7 +1,7 @@
 from peter.config import *
 from peter.misc import reset_idx
 
-def dismiss_employees(dataset):
+def layoff_employees(dataset):
   non_dismissed_employees = dataset.query('normalized_competence >= 0.4 and age <= 65').copy()
   non_dismissed_employees = reset_idx(non_dismissed_employees)
   return non_dismissed_employees
