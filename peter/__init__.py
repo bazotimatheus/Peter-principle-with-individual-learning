209672abd629d943fe1640eb2d50e80b01505aaf
Matheus Bazoti <102468065+bazotimatheus@users.noreply.github.com>
2023-11-23 19:55:02 -0300

    Update __init__.py

--- peter/__init__.py
+++ peter/__init__.py
@@ -1,6 +1,6 @@
 from peter.calculate_quantities import *
 from peter.config import *
-from peter.dismiss import *
+from peter.layoff import *
 from peter.generate_vectors import *
 from peter.hire import *
 from peter.misc import *
